6dce89400418a4cd803ffac37ecaa3cbc30d996e
shegzybrown <adeyeye.timilehin@yahoo.com>
2019-04-05 15:38:29 +0100

    Create Segun.php

---
+++ views/interns/Segun.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://cloudinary.com/console/media_library/asset/manage/summary/image/upload/20180208_121025.jpeg" alt="Adeyeye Olusegun">
+                        <h6>Adeyeye Olusegun</h6>
+                        <p>Front-end software developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/AdeyeyeOlusegu8" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/shegzybrown" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@adeyeye.timilehin1" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hi there! I am an embedded programmer, just started web design about 2 months ago. i am a quick learner but i am trusting this internship to put me on more pressure to learn faster. </p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>python |html|css</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
